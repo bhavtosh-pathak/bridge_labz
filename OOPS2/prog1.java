7d98e42f307fc6b6459ab7c3071d4561b724bc66
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2025-09-10 08:54:15 +0530

    Create prog1.java

---
+++ OOPS2/prog1.java
@@ -0,0 +1,24 @@
+package Class;
+
+public class PROG1 {
+    static class Book{
+        String title;
+        String author;
+        int price;
+
+        Book(){
+            this.title="null";
+            this.author="null";
+            this.price=0;
+        }
+        Book(String title,String author,int price){
+            this.title=title;
+            this.author=author;
+            this.price=price;
+        }
+
+    }
+    
+   
+    
+}
