3298cfb8d2ae6cce9f3ed1d07ead73a13f8882fa
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2025-09-16 23:01:12 +0530

    Create first.java

---
+++ string1/first.java
@@ -0,0 +1,22 @@
+package String_Level1;
+
+public class First {
+	public static void main(String[] args) {
+		String s="hello";
+		int count=0;
+		
+		
+			try {
+				while(true) {
+				s.charAt(count);
+				count++;
+			}
+			}
+			catch (Exception e) {
+				
+			}
+		System.out.println(count);
+		
+		
+	}
+}
