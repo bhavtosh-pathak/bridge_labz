16515a54b3a49bca47328122728f7f37e01c7bef
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2025-09-23 00:54:11 +0530

    Create prog7.java

---
+++ JAVASTRING/prog7.java
@@ -0,0 +1,13 @@
+package CORE;
+
+import java.util.Scanner;
+
+public class prog6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        System.out.println(a.compareTo(b));
+    }
+    
+}
