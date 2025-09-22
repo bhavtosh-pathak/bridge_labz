4ae5daf2c45cbbe5f06a32bb2acc19aeeae93f3e
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2025-09-23 00:50:26 +0530

    Create prog2.java

---
+++ JAVASTRING/prog2.java
@@ -0,0 +1,13 @@
+package CORE;
+
+import java.util.Scanner;
+
+public class prog2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = "bhavtosh";
+        StringBuilder sb = new StringBuilder(a);
+        System.out.println(sb.reverse());
+    }
+    
+}
