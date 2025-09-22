5ed34e639fd3c429f11c92138544526f22fc5d3a
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2025-09-22 22:16:25 +0530

    Create prog1.java

---
+++ DATEANDTIME/prog1.java
@@ -0,0 +1,8 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
+public class prog1 {
+    public static void main(String[] args) {
+    System.out.println(ZonedDateTime.now());
+    }
+}
