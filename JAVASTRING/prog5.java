7ac56c935773325c7be287b53fcf19bf799a1bac
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2025-09-23 00:51:58 +0530

    Create prog5.java

---
+++ JAVASTRING/prog5.java
@@ -0,0 +1,13 @@
+package CORE;
+
+import java.util.Scanner;
+
+public class prog5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.toUpperCase());
+    }
+    
+    
+}
