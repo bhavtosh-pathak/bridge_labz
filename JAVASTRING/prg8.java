515332fa66c0b55f3adc58b81a189ef3ec7929ca
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2025-09-23 00:52:56 +0530

    Create prg8.java

---
+++ JAVASTRING/prg8.java
@@ -0,0 +1,22 @@
+package CORE;
+
+import java.util.Scanner;
+
+public class prog8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char remove='h';
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==remove){
+                continue;
+            } 
+            else{
+                sb.append(str.charAt(i));
+            } 
+        }
+        System.out.println(sb.toString());
+    }
+    
+}
